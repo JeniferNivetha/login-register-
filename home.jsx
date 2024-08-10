b54cb9ba6f6e56aaf4893b7b7d210de133e02bef
JeniferNivetha <146548866+JeniferNivetha@users.noreply.github.com>
2024-08-10 20:18:16 +0530

    Add files via upload

---
+++ home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home(){
+    return(
+        <div className="d-flex justify-content-center align-items-center" >
+    
+        <h2><span>Welcome to Home Page</span></h2>
+        </div>
+    )
+}
+
+
+export default Home;
